6289c8a5f1366a1f1b2e59101068b3d95d6fdc29
Paulina <paulina.recko@gmail.com>
2018-05-27 12:16:02 +0200

    Sanitize tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
-  has_many :comments ,dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+
+
+  def sanitize_tags(tags = '')
+    tags.downcase.split(',').map(&:strip).uniq
+  end
 end
