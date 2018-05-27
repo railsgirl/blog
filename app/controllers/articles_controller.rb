64dd3d2c698c29f60b97eca7532bcbbcf3e26c70
Paulina <paulina.recko@gmail.com>
2018-05-27 11:51:52 +0200

    Articles tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(article_params)
+    @article.tags = article_params[:tags].split(',')
     if @article.save
       redirect_to @article
     else
