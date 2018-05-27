380f0da3675e8237c5b8dca05a85a1f6e5ba30e5
Paulina <paulina.recko@gmail.com>
2018-05-27 16:20:08 +0200

    Adds relation User Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
   def create
 
-    @article = Article.new(article_params)
+    @article = Article.new(article_params.merge(user_id: current_user.id))
     if @article.save
       redirect_to @article
     else
