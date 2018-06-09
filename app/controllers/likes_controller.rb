cb10d11a21682a1a8d7317fa3ad55fe585460dc9
Paulina <paulina.recko@gmail.com>
2018-06-09 12:19:20 +0200

    Adding unique restriction

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,7 @@
 class LikesController < ApplicationController
     def create
-        
+        article = Article.find(params[:article_id])
+        Like.create(user: current_user, article: article)
+        redirect_to article
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_090628) do
+ActiveRecord::Schema.define(version: 2018_06_09_100334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_06_09_090628) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id", "user_id"], name: "index_likes_on_article_id_and_user_id", unique: true
     t.index ["article_id"], name: "index_likes_on_article_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
