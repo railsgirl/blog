b8b0c140b83c61ea0bb6814149ad3c22bc74d81e
Paulina <paulina.recko@gmail.com>
2018-06-09 13:59:06 +0200

    Unlike mechanism

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,20 @@
 class LikesController < ApplicationController
+    before_action :find_article
+    
     def create
-        article = Article.find(params[:article_id])
-        Like.create(user: current_user, article: article)
-        redirect_to article
+        Like.create(user: current_user, article: @article)
+        redirect_to @article
+    end
+
+    def destroy
+        like = Like.find_by!(user: current_user, article: @article) 
+        like.destroy
+        redirect_to @article
+    end
+
+    private
+
+    def find_article
+        @article = Article.find(params[:article_id])
     end
 end
